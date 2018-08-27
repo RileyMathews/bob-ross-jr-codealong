704348a324c5c7287fde331e52bb95d46c6702aa
Riley Mathews <rileymathews80@gmail.com>
2018-08-27 13:23:24 -0500

    got button working

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,37 @@
-console.log("I am here! Its Javascript Time!")
+
+const elementFactory = (element, content) => {
+    const htmlElement = document.createElement(element)
+    // debugger
+    if (content !== null) {
+        htmlElement.textContent = content
+    }
+    return htmlElement
+}
 
 document.getElementById("js-clicker").addEventListener('click', () => {
-    console.log('you clicked the button')
     fetch('/birthday')
         .then(r => r.json())
         .then(response => {
-            console.log(response)
+            const birthdays = response
+            // create document fragment
+            const frag = document.createDocumentFragment()
+            birthdays.forEach(birthday => {
+                // create three text elements
+                nameElement = elementFactory('p', birthday.name)
+                dateElement = elementFactory('p', birthday.birthday)
+                greetingElement = elementFactory('p', birthday.greeting)
+
+                // create parent element
+                parentElement = elementFactory('div', null)
+                parentElement.appendChild(nameElement)
+                parentElement.appendChild(dateElement)
+                parentElement.appendChild(greetingElement)
+
+                // append parent element to fragment
+                frag.appendChild(parentElement)
+            })
+            document.getElementById('output').appendChild(frag)
+
         })
+
 })
